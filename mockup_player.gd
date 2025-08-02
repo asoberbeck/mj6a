b44fa5aaa2059590c164d7e2c30ad726504c2292
asoberbeck <1239294+asoberbeck@users.noreply.github.com>
2025-08-01 21:00:06 -0700

    input_lerp_weight to smooth movement

--- mockup_player.gd
+++ mockup_player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 100
+@export var input_lerp_weight = 0.1
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	look_at(get_global_mouse_position())
-	velocity = input_direction * speed
+	velocity = lerp(velocity, input_direction*speed, input_lerp_weight)
 
 func _physics_process(delta):
 	get_input()
